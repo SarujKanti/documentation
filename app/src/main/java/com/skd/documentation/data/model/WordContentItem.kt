042812ba41c22449800ef6065c959558637e540e
saruj <14skd12@gmail.com>
2026-02-08 03:15:05 +0530

    style changes

--- app/src/main/java/com/skd/documentation/data/model/WordContentItem.kt
+++ app/src/main/java/com/skd/documentation/data/model/WordContentItem.kt
@@ -2,5 +2,6 @@ package com.skd.documentation.data.model
 
 sealed class WordContentItem {
     data class Text(val value: String) : WordContentItem()
+
     data class Image(val resId: Int) : WordContentItem()
 }
