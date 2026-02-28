f030e1fcb39b841d43d0056f365219940aaaa6b2
saruj <14skd12@gmail.com>
2026-03-01 02:50:45 +0530

    new item added in repository

--- app/src/main/java/com/skd/documentation/data/repository/DocumentationRepository.kt
+++ app/src/main/java/com/skd/documentation/data/repository/DocumentationRepository.kt
@@ -29,7 +29,6 @@ class DocumentationRepository {
                     
                     WordContentItem.Text("The Home tab contains basic formatting tools."),
                     WordContentItem.Image(R.drawable.home_tab),
-
                     WordContentItem.Text("Use font options to customize text appearance."),
                     WordContentItem.Image(R.drawable.insert_tab),
                     WordContentItem.Text("Alignment tools help organize content."),
