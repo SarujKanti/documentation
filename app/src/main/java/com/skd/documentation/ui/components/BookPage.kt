3a3dd7a4f8895e53766f6e585a7d9c07e1cfb293
saruj <14skd12@gmail.com>
2026-04-11 19:06:21 +0530

    changes

--- app/src/main/java/com/skd/documentation/ui/components/BookPage.kt
+++ app/src/main/java/com/skd/documentation/ui/components/BookPage.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.F
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
