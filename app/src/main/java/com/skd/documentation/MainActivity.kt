ed38bd512878264ef9f3eaa1d98c23b102fde429
saruj <14skd12@gmail.com>
2026-05-21 22:01:36 +0530

    changes main activity code

--- app/src/main/java/com/skd/documentation/MainActivity.kt
+++ app/src/main/java/com/skd/documentation/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
                 darkScrim = Color.Transparent.toArgb()
             )
         )
+
         setContent {
             DocumentationTheme {
                 DocumentationScreen()
