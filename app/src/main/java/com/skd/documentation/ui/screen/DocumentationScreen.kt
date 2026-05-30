ed2644df11f6028cb7b2b1e804a01923279f0d16
saruj <14skd12@gmail.com>
2026-05-30 13:05:11 +0530

    added reasons of functions

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -58,7 +58,6 @@ fun DocumentationScreen(
             // derivedStateOf: tab labels only recompose when the page number
             // changes, NOT on every frame during a swipe gesture.
             val currentPage by remember { derivedStateOf { pagerState.currentPage } }
-
             // Scrollable tab row
             ScrollableTabRow(
                 selectedTabIndex = currentPage,
