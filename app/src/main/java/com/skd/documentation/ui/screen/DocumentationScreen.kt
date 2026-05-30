5d3a5b19a4279fa4a5ec5743754f7326d36da997
saruj <14skd12@gmail.com>
2026-05-30 13:05:06 +0530

    added reasons of functions

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -55,7 +55,6 @@ fun DocumentationScreen(
         key(selectedAppIndex) {
             val pagerState = rememberPagerState { selectedApp.sections.size }
             val appColor   = Color(selectedApp.primaryColor)
-
             // derivedStateOf: tab labels only recompose when the page number
             // changes, NOT on every frame during a swipe gesture.
             val currentPage by remember { derivedStateOf { pagerState.currentPage } }
