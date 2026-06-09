8469c69d192ec65c6512d366a3ee82601d944583
saruj <14skd12@gmail.com>
2026-06-09 21:24:31 +0530

    screen status changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -310,7 +310,6 @@ private fun AppSelectorSheet(
                 color    = Color(0xFF9CA3AF)
             )
         }
-
         
         HorizontalDivider(color = Color(0xFFF3F4F6))
         Spacer(Modifier.height(6.dp))
