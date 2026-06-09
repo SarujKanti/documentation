b775b23f92825bd28bba488052e62a21e837b078
saruj <14skd12@gmail.com>
2026-06-09 21:15:46 +0530

    screen status changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -213,7 +213,6 @@ private fun AppTopBar(
                     .size(40.dp)
                     .clip(CircleShape)
                     .clickable(
-                        
                         interactionSource = remember { MutableInteractionSource() },
                         indication        = null
                     ) { onMenuClick() }
