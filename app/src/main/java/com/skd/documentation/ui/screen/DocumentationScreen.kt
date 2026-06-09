a3ad9d37aa9b252fbadd15f12722d878100253b7
saruj <14skd12@gmail.com>
2026-06-09 21:16:17 +0530

    screen status changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -221,6 +221,7 @@ private fun AppTopBar(
                     modifier             = Modifier.size(width = 18.dp, height = 13.dp),
                     verticalArrangement  = Arrangement.SpaceBetween
                 ) {
+                    
                     repeat(3) {
                         Box(
                             modifier = Modifier
