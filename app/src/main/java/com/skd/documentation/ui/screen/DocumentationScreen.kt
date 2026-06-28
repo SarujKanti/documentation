24cd5e49920571b21230d612e8afedef4415bba3
saruj <14skd12@gmail.com>
2026-06-28 18:36:44 +0530

    screen splash changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -353,6 +353,7 @@ private fun AppSelectorSheet(
 
                 Spacer(Modifier.width(16.dp))
                 // Labels
+                
                 Column(modifier = Modifier.weight(1f)) {
                     Text(
                         text       = app.appName.removePrefix("MS "),
