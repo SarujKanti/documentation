75a49f673825e66fdf63216cf4de343c98a8796c
saruj <14skd12@gmail.com>
2026-07-02 22:50:51 +0530

    screen splash changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -358,7 +358,6 @@ private fun AppSelectorSheet(
                         fontSize   = 15.sp,
                         color      = if (isSelected) appColor else Color(0xFF111827)
                     )
-
                     
                     Text(
                         text     = "${app.sections.size} topics available",
