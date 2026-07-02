74a06b69690aee9f5dc0865faf819436b0b7420e
saruj <14skd12@gmail.com>
2026-07-02 21:22:29 +0530

    screen splash changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -349,7 +349,6 @@ private fun AppSelectorSheet(
                         modifier          = Modifier.size(24.dp)
                     )
                 }
-
                 Spacer(Modifier.width(16.dp))
                 // Labels
                 Column(modifier = Modifier.weight(1f)) {
