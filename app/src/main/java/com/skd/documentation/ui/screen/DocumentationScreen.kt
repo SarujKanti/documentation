b7fa32530caaeb40988e84dc7e179762ce963631
saruj <14skd12@gmail.com>
2026-08-20 00:01:28 +0530

    documentation screen changes  and commented code.

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -337,7 +337,9 @@ private fun AppSelectorSheet(
                     contentAlignment = Alignment.Center,
                     modifier         = Modifier
                         .size(46.dp)
-                        
+
+
+
                         .clip(CircleShape)
                         .background(
                             if (isSelected) appColor else appColor.copy(alpha = 0.10f)
