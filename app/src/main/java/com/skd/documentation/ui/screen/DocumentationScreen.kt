6fa265553427f67079d140918fd34d733f2aab00
saruj <14skd12@gmail.com>
2026-09-01 22:28:41 +0530

    documentation screen changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -355,7 +355,6 @@ private fun AppSelectorSheet(
 
                 Spacer(Modifier.width(16.dp))
                 // Labels
-                
                 Column(modifier = Modifier.weight(1f)) {
                     Text(
                         text       = app.appName.removePrefix("MS "),
