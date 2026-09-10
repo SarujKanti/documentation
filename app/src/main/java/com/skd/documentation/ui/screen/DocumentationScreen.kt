2cc86d3c9c01cb6059d7ae2e00123fc3b7ec0c5a
saruj <14skd12@gmail.com>
2026-09-11 00:05:24 +0530

    documentation screen changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -361,8 +361,6 @@ private fun AppSelectorSheet(
                         
                         color      = if (isSelected) appColor else Color(0xFF111827)
                     )
-
-
                     Text(
                         text     = "${app.sections.size} topics available",
                         fontSize = 11.sp,
