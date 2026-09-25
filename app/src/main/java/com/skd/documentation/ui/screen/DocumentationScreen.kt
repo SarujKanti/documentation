d80b0a381803a2a9a7d52666e360631e6828febb
saruj <14skd12@gmail.com>
2026-09-25 20:56:49 +0530

    documentation screen changes

--- app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
+++ app/src/main/java/com/skd/documentation/ui/screen/DocumentationScreen.kt
@@ -352,7 +352,6 @@ private fun AppSelectorSheet(
                         modifier          = Modifier.size(24.dp)
                     )
                 }
-
                 
 
                 Spacer(Modifier.width(16.dp))
